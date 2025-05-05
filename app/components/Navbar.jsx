0b49518efa472e3f26350c09207da45a71afab38
Thanat <bztzn512@gmail.com>
2025-05-05 18:12:30 -0400

    addedSidebar

---
+++ app/components/Navbar.jsx
@@ -0,0 +1,113 @@
+import Image from "next/image";
+import { assets } from "@/assets/assets";
+import { useRef } from "react";
+
+const Navbar = () => {
+  const sideMenuRef = useRef();
+
+  const openMenu = () => {
+    sideMenuRef.current.style.transform = "translateX(-16rem)";
+  };
+
+  const closeMenu = () => {
+    sideMenuRef.current.style.transform = "translateX(16rem)";
+    sideMenuRef.current.style = "transition duration-600 delay-100 ease-out";
+  };
+  return (
+    <>
+      <nav className="w-full fixed px-5 lg:px-8 xl:px-[8%] py-4 flex flex-center justify-between z-50">
+        <a href="#top">
+          <Image
+            src={assets.logo}
+            alt="logo"
+            className="w-28 cursor-pointer mr-14"
+          />
+        </a>
+        <ul className="hidden md:flex item-center gap-6 lg:gap-8 rounded-full px-12 py-3 bg-white shadow-sm bg-opacity-50">
+          <li>
+            <a href="#top" className="font-Ovo">
+              Home
+            </a>
+          </li>
+          <li>
+            <a href="#about" className="font-Ovo">
+              About me
+            </a>
+          </li>
+          <li>
+            <a href="#services" className="font-Ovo">
+              Services
+            </a>
+          </li>
+          <li>
+            <a href="#works" className="font-Ovo">
+              My works
+            </a>
+          </li>
+          <li>
+            <a href="#contact" className="font-Ovo">
+              Contact Me
+            </a>
+          </li>
+        </ul>
+        <div className="flex items-center gap-4">
+          <button>
+            <Image src={assets.moon_icon} alt="moon-icon" className="w-6" />
+          </button>
+          <a
+            href="#contact"
+            className="hidden lg:flex items-center gap-3 px-10 py-2.5 border border-gray-500 rounded-full ml-4"
+          >
+            Contact
+            <Image src={assets.arrow_icon} alt="arrow" className="w-3" />
+          </a>
+          <button className="block md:hidden" onClick={openMenu}>
+            <Image src={assets.menu_black} alt="icon-black" className="w-6" />
+          </button>
+        </div>
+
+        {/* moblie Menu */}
+        <ul
+          ref={sideMenuRef}
+          className="flex md:hidden flex-col gap-4 py-20 px-10 fixed -right-64 top-0 bottom-0 w-64 z-50 h-screen bg-rose-50 transition duration-600 delay-100 ease-out"
+        >
+          <div className="absolute right-6 top-6" onClick={closeMenu}>
+            <Image
+              src={assets.close_black}
+              alt=""
+              className="w-5 cursor-pointer"
+            />
+          </div>
+
+          <li>
+            <a href="#top" className="font-Ovo" onClick={closeMenu}>
+              Home
+            </a>
+          </li>
+          <li>
+            <a href="#about" className="font-Ovo" onClick={closeMenu}>
+              About me
+            </a>
+          </li>
+          <li>
+            <a href="#services" className="font-Ovo" onClick={closeMenu}>
+              Services
+            </a>
+          </li>
+          <li>
+            <a href="#works" className="font-Ovo" onClick={closeMenu}>
+              My works
+            </a>
+          </li>
+          <li>
+            <a href="#contact" className="font-Ovo" onClick={closeMenu}>
+              Contact Me
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </>
+  );
+};
+
+export default Navbar;
