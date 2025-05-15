c00d9cdb07ad65604a3eddd89edf59106429f141
Thanat <bztzn512@gmail.com>
2025-05-15 10:52:39 -0400

    addedContact

---
+++ app/components/Contact.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Contact = () => {
+  return (
+    <div
+      id="contact"
+      className='w-full px-[12%] py-10 scroll-mt-20 bg-[url("/footer-bg-color.png")] bg-no-repeat bg-center bg-[lenth:90%_auto]'
+    >
+      <h4 className="text-center mb-2 text-lg font-Ovo">Contact Me</h4>
+      <h2 className="text-center text-5xl font-Ovo">Get In Touch</h2>
+
+      <p className="text-center max-w-2xl mx-auto mt-5 mb-12 font-Ovo">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aliquam culpa
+        rerum nostrum exercitationem sint corrupti.
+      </p>
+    </div>
+  );
+};
+
+export default Contact;
